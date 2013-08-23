435e7000a5170b84dc506cb8fe1c5d2b6023f99c
Matthew Skala <mskala@ansuz.sooke.bc.ca>
2013-08-23 16:08:08 -0500

    "gloom" and its many dependencies

--- idsgrep/match.c
+++ idsgrep/match.c
@@ -1,6 +1,6 @@
 /*
  * Matching algorithm for IDSgrep
- * Copyright (C) 2012  Matthew Skala
+ * Copyright (C) 2012, 2013  Matthew Skala
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
